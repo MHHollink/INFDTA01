8fff128a6181c13046763e372f8fbab89321ce94
mjollnir94 <marcel_Hollink@hotmail.com>
2016-03-11 13:54:05 +0100

    updated models for database

--- src/main/java/nl/hro/dta01/lesson/two/model/Item.java
+++ src/main/java/nl/hro/dta01/lesson/two/model/Item.java
@@ -1,4 +1,23 @@
 package nl.hro.dta01.lesson.two.model;
 
+import nl.hro.dta01.lesson.two.model.enums.Genre;
+
+import java.util.HashSet;
+
 public class Item {
+
+    public static class Column {
+        public static final String ID = "item_id";
+        public static final String NAME = "title";
+        public static final String REALEASE_DATE = "release_date";
+        public static final String URL = "imdb_url";
+        public static final String GENRES = "genres";
+    }
+
+    private Integer id;
+    private String name;
+    private String date;
+    private String imdbUrl;
+    private HashSet<Genre> genres;
+
 }
---
+++ src/main/java/nl/hro/dta01/lesson/two/model/enums/Gender.java
@@ -0,0 +1,5 @@
+package nl.hro.dta01.lesson.two.model.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
