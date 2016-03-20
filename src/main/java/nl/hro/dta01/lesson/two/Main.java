5c0d3940110a61153694dbe9d6ea9020be907769
mjollnir94 <marcel_Hollink@hotmail.com>
2016-03-20 19:41:04 +0100

    Removed all Errors from Main (this commit may be reverted if you want)

--- src/main/java/nl/hro/dta01/lesson/two/Main.java
+++ src/main/java/nl/hro/dta01/lesson/two/Main.java
@@ -1,7 +1,6 @@
 package nl.hro.dta01.lesson.two;
 
 import nl.hro.dta01.lesson.two.importer.MovieLensDataImporter;
-import nl.hro.dta01.lesson.two.importer.UserItemDataImporter;
 import nl.hro.dta01.lesson.two.matrix.*;
 import nl.hro.dta01.lesson.two.model.Tuple;
 import nl.hro.dta01.lesson.two.model.UserPreference;
@@ -10,25 +9,17 @@ import java.util.*;
 
 public class Main {
 
-    private static Map<Integer, UserPreference> userPrefrences;
+    private static Map<Integer, UserPreference> userPreferences;
     private static boolean dataIsComplete;
-    private static int numNearestNeighbours = 5;
-    private static double similarityThreshold = 0.35;
 
     public static void main(String[] args) {
         dataIsComplete = true;
-        if(args.length > 3) {
-            if (args[2] != null)
-                numNearestNeighbours = Integer.parseInt(args[2]);
-            if (args[3] != null)
-                similarityThreshold = Double.parseDouble(args[3]);
-        }
 
-        //userPrefrences = UserItemDataImporter.ImportUserItemDataIntoUserPreferences();    // Loads the userItem.data file into the TreeMap
-        userPrefrences = MovieLensDataImporter.ImportUserItemDataIntoUserPreferences();    // Loads the ml-100k/u.data file into the TreeMap
+        //userPreferences = UserItemDataImporter.ImportUserItemDataIntoUserPreferences();    // Loads the userItem.data file into the TreeMap
+        userPreferences = MovieLensDataImporter.ImportUserItemDataIntoUserPreferences();    // Loads the ml-100k/u.data file into the TreeMap
 
-        for (Integer i : userPrefrences.keySet() ) {
-            System.out.println(userPrefrences.get(i));
+        for (Integer i : userPreferences.keySet() ) {
+            System.out.println(userPreferences.get(i));
         }
 
         System.exit(0);
@@ -36,10 +27,10 @@ public class Main {
         /*
         Show the data for each user
          */
-        for (Integer key : userPrefrences.keySet()){
+        for (Integer key : userPreferences.keySet()){
             System.out.println("User " + key + ":");
 
-            UserPreference userPreference = userPrefrences.get(key);
+            UserPreference userPreference = userPreferences.get(key);
             for (Integer productKey : userPreference.getRatings().keySet()){
                 System.out.println("\tProductId: " + productKey + ", value: " + userPreference.getRatings().get(productKey));
             }
@@ -49,9 +40,9 @@ public class Main {
         Searches the imported data and checks if "holes" exist. If there are holes found in the data, the program saves the dataIsComplete as FALSE.
         When dataIsComplete is false the program will use the CosineSimilarity algoritm
          */
-        for (int i = 1; i < userPrefrences.size(); i++) {
-            UserPreference a = userPrefrences.get(i);
-            UserPreference b = userPrefrences.get(i+1);
+        for (int i = 1; i < userPreferences.size(); i++) {
+            UserPreference a = userPreferences.get(i);
+            UserPreference b = userPreferences.get(i+1);
 
             for (Integer productKey : a.getRatings().keySet()){
                 if(!b.getRatings().keySet().contains(productKey)){
@@ -67,8 +58,8 @@ public class Main {
         System.out.println("Finding 3 nearest neighbours of user 7 using the PearsonCoefficient algoritm");
         List<Tuple<Integer, Double>> similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(7, 3, 0.35, PearsonCoefficient.class);
         System.out.println("The 3 nearest neighbours are:");
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            System.out.println("\t" + similaritiesToTargetUser.get(i).getX() + ": " + similaritiesToTargetUser.get(i).getY());
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            System.out.println("\t" + aSimilaritiesToTargetUser.getX() + ": " + aSimilaritiesToTargetUser.getY());
         }
 
         /*
@@ -77,8 +68,8 @@ public class Main {
         System.out.println("Finding 3 nearest neighbours of user 7 using the Euclidian algoritm");
         similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(7, 3, 0.35, EuclideanDistance.class);
         System.out.println("The 3 nearest neighbours are:");
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            System.out.println("\t" + similaritiesToTargetUser.get(i).getX() + ": " + similaritiesToTargetUser.get(i).getY());
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            System.out.println("\t" + aSimilaritiesToTargetUser.getX() + ": " + aSimilaritiesToTargetUser.getY());
         }
 
         /*
@@ -87,8 +78,8 @@ public class Main {
         System.out.println("Finding 3 nearest neighbours of user 7 using the Cosine algoritm");
         similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(7, 3, 0.35, CosineSimilarity.class);
         System.out.println("The 3 nearest neighbours are:");
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            System.out.println("\t" + similaritiesToTargetUser.get(i).getX() + ": " + similaritiesToTargetUser.get(i).getY());
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            System.out.println("\t" + aSimilaritiesToTargetUser.getX() + ": " + aSimilaritiesToTargetUser.getY());
         }
 
         /*
@@ -101,46 +92,46 @@ public class Main {
         // Get 3 nearest neighbours for user 7 using the Pearson Coefficient
         similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(7, 3, 0.35, PearsonCoefficient.class);
         System.out.println("The 3 nearest neighbours are:");
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            System.out.println("\t" + similaritiesToTargetUser.get(i).getX() + ": " + similaritiesToTargetUser.get(i).getY());
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            System.out.println("\t" + aSimilaritiesToTargetUser.getX() + ": " + aSimilaritiesToTargetUser.getY());
         }
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 101);
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 103);
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 106);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 101);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 103);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 106);
 
         // Get 3 nearest neighbours for user 4 using the Pearson Coefficient
         similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(4, 3, 0.35, PearsonCoefficient.class);
         System.out.println("The 3 nearest neighbours are:");
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            System.out.println("\t" + similaritiesToTargetUser.get(i).getX() + ": " + similaritiesToTargetUser.get(i).getY());
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            System.out.println("\t" + aSimilaritiesToTargetUser.getX() + ": " + aSimilaritiesToTargetUser.getY());
         }
 
-        predictUserRatingForItem(4, similaritiesToTargetUser, userPrefrences, 101);
+        predictUserRatingForItem(4, similaritiesToTargetUser, userPreferences, 101);
 
         /*
         Insert rating for product 106 and user 7 into the dataset
          */
         System.out.println("Insert rating for product 106 and user 7 into the dataset");
-        userPrefrences.get(7).getRatings().put(106, 2.8);
+        userPreferences.get(7).getRatings().put(106, 2.8);
 
         // Get 3 nearest neighbours for user 7 using the Pearson Coefficient
         similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(7, 3, 0.35, PearsonCoefficient.class);
         System.out.println("Recalculate the predicted ratings for products 101 and 103 for user 7");
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 101);
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 103);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 101);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 103);
 
 
         /*
         Insert rating for product 106 and user 7 into the dataset
          */
         System.out.println("Update rating for product 106 and user 7 in the dataset");
-        userPrefrences.get(7).getRatings().put(106, (double)5);
+        userPreferences.get(7).getRatings().put(106, (double)5);
 
         // Get 3 nearest neighbours for user 7 using the Pearson Coefficient
         similaritiesToTargetUser = getNearestNeighboursUsingAlgoritm(7, 3, 0.35, PearsonCoefficient.class);
         System.out.println("Recalculate the predicted ratings for products 101 and 103 for user 7");
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 101);
-        predictUserRatingForItem(7, similaritiesToTargetUser, userPrefrences, 103);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 101);
+        predictUserRatingForItem(7, similaritiesToTargetUser, userPreferences, 103);
     }
 
     /*
@@ -148,8 +139,8 @@ public class Main {
      */
     public static void predictUserRatingForItem(int targetUser_id, List<Tuple<Integer, Double>> similaritiesToTargetUser, Map<Integer, UserPreference> userPrefrences, int product_item) {
         System.out.println("The " + similaritiesToTargetUser.size() + " nearest neighbours are:");
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            System.out.println("\t" + similaritiesToTargetUser.get(i).getX() + ": " + similaritiesToTargetUser.get(i).getY());
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            System.out.println("\t" + aSimilaritiesToTargetUser.getX() + ": " + aSimilaritiesToTargetUser.getY());
         }
 
         // Get the ratings and similarities needed for the predicted rating calculation
@@ -157,13 +148,6 @@ public class Main {
         // Calculate the predicted rating
         double predictedUserRatingForProduct = getPredictedRating(nearestNeighboursRatingForProduct);
         System.out.println("Predicted rating for user " + targetUser_id + " for item " + product_item + " is " + predictedUserRatingForProduct);
-    }
-
-    /*
-    Get nearest neighbours using the strategy chosen by the program
-     */
-    public static List<Tuple<Integer, Double>> getNearestNeighbours(int targetUser_id, int numOfNeighbours, double threshold) {
-        return getNearestNeighboursUsingAlgoritm(targetUser_id, numOfNeighbours, threshold, null);
     }
 
     /*
@@ -177,8 +161,8 @@ public class Main {
         }
 
         // Loop through all users, unless the user is the target user
-        for (int i = 1; i <= userPrefrences.size(); i++) {
-            if((i <= userPrefrences.size()) && (i != targetUser_id)) {
+        for (int i = 1; i <= userPreferences.size(); i++) {
+            if((i <= userPreferences.size()) && (i != targetUser_id)) {
                 double similarity;
                 if(strategy == null)    // Use the strategy chosen by the program(Cosine for sparse data, Pearson otherwise)
                     similarity = calculateSimilarity(targetUser_id, (i));
@@ -188,9 +172,7 @@ public class Main {
 
                 if(similarity > threshold) {    // If the similarity is higher than the threshold, update the list
                     Optional<Tuple<Integer, Double>> firstZeroSimilarityInList =
-                            similaritiesToTargetUser.stream().filter((Tuple<Integer, Double> similarityToUserOne) -> {
-                        return similarityToUserOne.getY() == (double)-1;
-                    }).findFirst();
+                            similaritiesToTargetUser.stream().filter((Tuple<Integer, Double> similarityToUserOne) -> similarityToUserOne.getY() == (double)-1).findFirst();
 
                     try {
                         // Try to find the first zero similarity in the list and update it with the new similarity
@@ -221,8 +203,8 @@ public class Main {
     }
 
     public static double calculateSimilarityUsingGivenAlgorithm(int userOne_id, int userTwo_id, Class<? extends SimilarityStrategy> strategy){
-        UserPreference personOne = userPrefrences.get(userOne_id);
-        UserPreference personTwo = userPrefrences.get(userTwo_id);
+        UserPreference personOne = userPreferences.get(userOne_id);
+        UserPreference personTwo = userPreferences.get(userTwo_id);
 
         if(strategy == null)
             return SimilarityMatrix.getInstance().calculateSimilarity(dataIsComplete, personOne, personTwo);
@@ -236,9 +218,9 @@ public class Main {
      */
     public static double getPredictedRating(List<Tuple<Double, Double>> userRatings){
         double a = 0, b = 0;
-        for (int i = 0; i < userRatings.size(); i++) {
-            a += userRatings.get(i).getY()*userRatings.get(i).getX();
-            b += userRatings.get(i).getY();
+        for (Tuple<Double, Double> userRating : userRatings) {
+            a += userRating.getY() * userRating.getX();
+            b += userRating.getY();
         }
         return a/b;
     }
@@ -249,10 +231,10 @@ public class Main {
     public static List<Tuple<Double, Double>> getRatingAndSimilarityFromNeighboursAboutProduct(List<Tuple<Integer, Double>> similaritiesToTargetUser, Map<Integer, UserPreference> userPrefrences, int product_id){
         List<Tuple<Double, Double>> returnList = new ArrayList<>();
 
-        for (int i = 0; i < similaritiesToTargetUser.size(); i++) {
-            int user_id = similaritiesToTargetUser.get(i).getX();
-            double similarityToUser = similaritiesToTargetUser.get(i).getY();
-            if(userPrefrences.get(user_id).getRatings().get(product_id) != null) {
+        for (Tuple<Integer, Double> aSimilaritiesToTargetUser : similaritiesToTargetUser) {
+            int user_id = aSimilaritiesToTargetUser.getX();
+            double similarityToUser = aSimilaritiesToTargetUser.getY();
+            if (userPrefrences.get(user_id).getRatings().get(product_id) != null) {
                 double user_rating = userPrefrences.get(user_id).getRatings().get(product_id);
 
                 returnList.add(new Tuple<>(user_rating, similarityToUser));
