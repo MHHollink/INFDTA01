888b4f43d9bcb850ce600cdf5c4099c546be1c4c
mjollnir94 <marcel_Hollink@hotmail.com>
2016-04-10 16:03:11 +0200

    some imports and stuff

--- src/main/java/nl/hro/dta01/lesson/four/Main.java
+++ src/main/java/nl/hro/dta01/lesson/four/Main.java
@@ -13,12 +13,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.String.format;
-import static java.lang.System.currentTimeMillis;
-import static java.lang.System.out;
 import static nl.hro.dta01.lesson.four.Importer.loadDataMovieLens;
-import static nl.hro.dta01.lesson.four.matrix.SlopeOne.calculateDeviation;
 import static nl.hro.dta01.lesson.four.Importer.loadDataUserItem;
-import static nl.hro.dta01.lesson.four.matrix.SlopeOne.*;
+import static nl.hro.dta01.lesson.four.matrix.SlopeOne.calculateDeviation;
+import static nl.hro.dta01.lesson.four.matrix.SlopeOne.updateDeviationModel;
 
 public class Main {
 
@@ -37,15 +35,15 @@ public class Main {
             itemDataSet.add(i);
         }
 
-        out.println( "loading data..." );
+        System.out.println( "loading data..." );
         start = System.currentTimeMillis(); // START LOADING
 
         userRatings = loadDataMovieLens();
         end = System.currentTimeMillis();   // ENDED LOADING
-        out.println( format("loading took %f seconds", (end-start) / 1000.0 ) );
+        System.out.println( format("loading took %f seconds", (end-start) / 1000.0 ) );
 
         deviationModels = new ConcurrentHashMap<>();
-        out.println( "calculating..." );
+        System.out.println( "calculating..." );
         start = System.currentTimeMillis(); // START CALCULATING DEVIATION
         itemDataSet.parallelStream()
                 .forEach(item -> {
@@ -58,28 +56,30 @@ public class Main {
                     });
                 });
         end = System.currentTimeMillis();  // ENDED CALCULATING DEVIATION
-        out.println(format("calculating took %f seconds", (end-start) / 1000.0 ) );
+        System.out.println(format("calculating took %f seconds", (end-start) / 1000.0 ) );
 
         /** SMALL SET **/
-//        out.println(format("We predict that user %d would rate item %d with a %f rating",7,101,predictOne(7,101)));
-//        out.println(format("We predict that user %d would rate item %d with a %f rating", 7,103,predictOne(7,103)));
-//        out.println(format("We predict that user %d would rate item %d with a %f rating", 7,106,predictOne(7,106)));
-//        out.println(format("We predict that user %d would rate item %d with a %f rating", 3,103,predictOne(3,103)));
-//        out.println(format("We predict that user %d would rate item %d with a %f rating", 3,105,predictOne(3,105)));
-
-        // TODO, update user 3 rating, recalculate user 7.
+//        System.out.println(format("We predict that user %d would rate item %d with a %f rating",7,101,predictOne(7,101)));
+//        System.out.println(format("We predict that user %d would rate item %d with a %f rating", 7,103,predictOne(7,103)));
+//        System.out.println(format("We predict that user %d would rate item %d with a %f rating", 7,106,predictOne(7,106)));
+//        System.out.println(format("We predict that user %d would rate item %d with a %f rating", 3,103,predictOne(3,103)));
+//        System.out.println(format("We predict that user %d would rate item %d with a %f rating", 3,105,predictOne(3,105)));
+//
+//        // Add new rating and update deviations
+        deviationModels = addRating(3,105,4.0);
+//
+//        System.out.println(predictList(7, 3));
 
         /** BIG SET **/
 
-        out.println( "predicting..." );
-        start = currentTimeMillis();
+        System.out.println( "predicting..." );
+        start = System.currentTimeMillis();
         List< Tuple<Integer, Double>> predictions = predictList(186, 5);
-        end = currentTimeMillis();
-        out.println(format("predicting took %f seconds", (end-start) / 1000.0 ) );
+        end = System.currentTimeMillis();
+        System.out.println(format("predicting took %f seconds", (end-start) / 1000.0 ) );
         System.out.println(predictions);
 
-        // Add new rating and update deviations
-        Map<String,DeviationModel> newDeviations = addRating(3,103,5.0, userRatings, deviationModels, itemDataSet);
+
     }
 
     /**
@@ -108,14 +108,28 @@ public class Main {
         return ratings;
     }
 
-    public static Map<String, DeviationModel> addRating(int userId, int itemId, double rating, Map<Integer, User> data, Map<String, DeviationModel> deviations, List<Integer> itemDataset) {
-        Map<String, DeviationModel> newDeviationsModels = deviations;
-        System.out.println("============= updated deviationModels ===============");
-
-        User user = data.get(userId);
+    /**
+     * adds one rating by a user and updates all the deviations
+     * @param userId    = user submitted the new rating
+     * @param itemId    = the item that is rated
+     * @param rating    = the rating it self
+     * @return  a new map containing item-item as key and a {@link DeviationModel}
+     */
+    public static Map<String, DeviationModel> addRating(int userId, int itemId, double rating) {
+        User user = userRatings.get(userId);
         user.addRating(itemId, rating);
 
-        deviations.entrySet().stream().filter(entry -> ((entry.getValue().getItemIdA() == itemId) || (entry.getValue().getItemIdB() == itemId)) && user.hasRated(entry.getValue().getItemIdA()) && user.hasRated(entry.getValue().getItemIdB())).forEach(entry -> {
+        deviationModels.entrySet().stream().filter(entry -> {
+            if ((entry.getValue().getItemIdA() == itemId) || (entry.getValue().getItemIdB() == itemId)) {
+                if (user.hasRated(entry.getValue().getItemIdA()) && user.hasRated(entry.getValue().getItemIdB())) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }).forEach(entry -> {
             List<Tuple<Double, Double>> newRatings = new ArrayList<>();
             double ratingItemA = user.getRatingForItem(entry.getValue().getItemIdA());
             double ratingItemB = user.getRatingForItem(entry.getValue().getItemIdB());
@@ -123,10 +137,9 @@ public class Main {
             newRatings.add(new Tuple<>(ratingItemA, ratingItemB));
 
             DeviationModel updatedDeviationModel = updateDeviationModel(entry.getValue(), newRatings);
-            newDeviationsModels.put(entry.getKey(), updatedDeviationModel);
-            System.out.println(entry.getValue().getItemIdA() + "-" + entry.getValue().getItemIdB() + ": " + updatedDeviationModel.toString());
+            deviationModels.put(entry.getKey(), updatedDeviationModel);
         });
-        return newDeviationsModels;
+        return deviationModels;
     }
 
 
